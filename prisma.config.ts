074c9da955885d309970c5f733d26ee3af466610
Nguyen Thanh Danh <139749812+DanhNguyen49003@users.noreply.github.com>
2026-06-16 14:00:09 +0700

    refactor: comment out unused imports and configuration in prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,11 @@
-import path from 'path';
-import { defineConfig } from 'prisma/config';
-import { PrismaPg } from '@prisma/adapter-pg';
+// import path from 'path';
+// import { defineConfig } from 'prisma/config';
+// import { PrismaPg } from '@prisma/adapter-pg';
 
-export default defineConfig({
-  // earlyAccess: true,
-  schema: path.join('prisma', 'schema.prisma'),
-  datasource: {
-    url: process.env.DATABASE_URL as string,
-  },
-})
+// export default defineConfig({
+//   // earlyAccess: true,
+//   schema: path.join('prisma', 'schema.prisma'),
+//   datasource: {
+//     url: process.env.DATABASE_URL as string,
+//   },
+// })
