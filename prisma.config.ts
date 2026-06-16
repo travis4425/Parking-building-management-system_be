ce95fe8add484aa6154200a1ede76da2abc3acbf
travis4425 <sockhanh4425@gmail.com>
2026-06-16 13:47:57 +0700

    prisma migrate adapter

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,6 +5,15 @@ import { PrismaPg } from '@prisma/adapter-pg';
 export default defineConfig({
   // earlyAccess: true,
   schema: path.join('prisma', 'schema.prisma'),
+  migrate: {
+    async adapter() {
+      const { Pool } = await import('pg')
+      const pool = new Pool({
+        connectionString: process.env.DIRECT_URL,
+      })
+      return new PrismaPg(pool)
+    },
+  },
   datasource: {
     url: process.env.DATABASE_URL as string,
   },
