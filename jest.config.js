7aa4211052ab1d29dec4957ea85af556b7aebbb2
travis4425 <sockhanh4425@gmail.com>
2026-07-03 15:51:00 +0700

    Cập nhật jest.config: dùng moduleNameMapper trỏ uuid về shim CJS

--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,7 @@ module.exports = {
     ...tsJestTransformCfg,
   },
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
+  moduleNameMapper: {
+    "^uuid$": "<rootDir>/__mocks__/uuid.js",
+  },
 };
